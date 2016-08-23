5494532223e17b2a2b38610c49be5063c5ad7d5f
simeonlee <simeonhlee@gmail.com>
2016-08-22 20:19:19 -0700

    Urls redirecting to login

--- shortly.js
+++ shortly.js
@@ -2,7 +2,7 @@ var express = require('express');
 var util = require('./lib/utility');
 var partials = require('express-partials');
 var bodyParser = require('body-parser');
-
+var session = require('express-session');
 
 var db = require('./app/config');
 var Users = require('./app/collections/users');
@@ -21,65 +21,120 @@ app.use(bodyParser.json());
 // Parse forms (signup/login)
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/public'));
+// app.use(session({
+//   secret: 'simeon',
+//   resave: false,
+//   saveUnitialized: true,
+//   cookie: {secure: true}
+// }));
+
+// function restrict(req, res, next) {
+//   if (req.session.user) {
+//     next();
+//   } else {
+//     req.session.error = 'Access denied!';
+//     res.redirect('/login');
+//   }
+// }
 
 
 app.get('/', 
 function(req, res) {
-  res.render('index');
+  if (req.session.user) {
+    res.render('index');
+  } else {
+    res.redirect('/login');
+  }
+});
+    
+
+app.get('/login', function(req, res) {
+  //test that user is signed in
+  res.render('login');
+});
+
+app.get('/signup', function(req, res) {
+  res.render('signup');
 });
 
 app.post('/signup', function(req, res) {
-  console.log('We\'ve signed up the user!');
   new User({ username: req.body.username, password: req.body.password }).fetch().then(function(found) {
     if (found) {
       res.status(200).send(found.attributes);
     }
+    Users.create({
+      username: req.body.username,
+      password: req.body.password,
+    })
+    .then(function(newUser) {
+      res.set('location', '/');
+      res.status(200).send(newUser);
+    });
   });
-  console.log('We\'ve created the user in the database!');
+  // console.log('We\'ve created the user in the database!');
+  // User.where('username', 'apple').fetch().then(function(user) {
+  //   console.log('We found ' + user);
+  //   console.log(user);
+  // }).catch(function(err) {
+  //   console.log(err);
+  // });
 });
+
 
 app.get('/create', 
 function(req, res) {
-  res.render('index');
+  if (req.session.user) {
+    res.render('index');
+  } else {
+    res.redirect('/login');
+  }
 });
 
 app.get('/links', 
 function(req, res) {
-  Links.reset().fetch().then(function(links) {
-    res.status(200).send(links.models);
-  });
+  if (req.session.user) {
+    Links.reset().fetch().then(function(links) {
+      res.status(200).send(links.models);
+    });
+  } else {
+    res.redirect('/login');
+  }
 });
 
 app.post('/links',
 function(req, res) {
-  var uri = req.body.url;
+  if (req.session.user) {
+    var uri = req.body.url;
 
-  if (!util.isValidUrl(uri)) {
-    console.log('Not a valid url: ', uri);
-    return res.sendStatus(404);
-  }
-
-  new Link({ url: uri }).fetch().then(function(found) {
-    if (found) {
-      res.status(200).send(found.attributes);
-    } else {
-      util.getUrlTitle(uri, function(err, title) {
-        if (err) {
-          console.log('Error reading URL heading: ', err);
-          return res.sendStatus(404);
-        }
-
-        Links.create({
-          url: uri,
-          title: title,
-          baseUrl: req.headers.origin
-        })
-        .then(function(newLink) {
-          res.status(200).send(newLink);
-        });
-      });
+    if (!util.isValidUrl(uri)) {
+      console.log('Not a valid url: ', uri);
+      return res.sendStatus(404);
     }
-  });
+
+    new Link({ url: uri }).fetch().then(function(found) {
+      if (found) {
+        res.status(200).send(found.attributes);
+      } else {
+        util.getUrlTitle(uri, function(err, title) {
+          if (err) {
+            console.log('Error reading URL heading: ', err);
+            return res.sendStatus(404);
+          }
+
+          Links.create({
+            url: uri,
+            title: title,
+            baseUrl: req.headers.origin
+          })
+          .then(function(newLink) {
+            res.status(200).send(newLink);
+          });
+        });
+      }
+    });
+  } else {
+    res.redirect('/login');
+  }
 });
 
 /************************************************************/
